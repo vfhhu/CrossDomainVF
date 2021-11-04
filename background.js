4b11f38abeeb234aaaec9210e85f085cf8f73358
vfhhu <newvfhhu>
2021-11-04 15:41:55 +0800

    update

--- background.js
+++ background.js
@@ -1,13 +1,8 @@
 //---------------------------------------------------
 
 
-//if you need to change more,add a new Listener
-  
  //set  Access-Control-Allow-Origin
  chrome.webRequest.onHeadersReceived.addListener(function(details) {
 		details.responseHeaders.push({name:'Access-Control-Allow-Origin',value:"*"});
-		console.log(details.responseHeaders)
 		  return {responseHeaders:details.responseHeaders};
  },{urls: ["<all_urls>"]}, ["responseHeaders","blocking"]);
-
-//---------------------------------------------------
